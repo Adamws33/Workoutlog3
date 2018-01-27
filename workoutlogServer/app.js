a32d10155655f6db20458959bde92e240b4ed574
Adam Smith <adamws33@gmail.com>
2018-01-27 13:49:26 -0500

    middleware Headers

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+// var bodyParcer = require('body-parcer');
+
+app.use(require('./middleware/headers'))
 
 app.use('/api/test', function(req,res){
-  res.send("Hello World");
-});
+  res.send("Hello World")
+})
 
 app.listen(3000, function(){
-    console.log('app is open on 3000');
-});
+    console.log('app is open on 3000')
+})
