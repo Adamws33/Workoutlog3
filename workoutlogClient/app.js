160d602d543bc5d1ead041d22ba809d74f24b509
Adam Smith <adamws33@gmail.com>
2018-01-28 15:53:48 -0500

    added sign up info

--- workoutlogClient/app.js
+++ workoutlogClient/app.js
@@ -1,5 +1,41 @@
 // wait for Jquery to be ready
-$(document).ready(function(){
+$(function(){
+  var WorkoutLog = (function($, undefined){
+
+      //private
+      var API_BASE = "http://localhost:3000/api/";
+      var userDefinitions = [];
+
+      var setAuthHeader = function(sessionToken){
+        window.localStorage.setItem("sessionToken", sessionToken);
+        //set the authorization header
+        //This can be done on individual calls
+        //here we showcase ajaxSetup as a global tool
+        $.ajaxSetup({
+          "headers": {
+            "Authorization": sessionToken
+          }
+        });
+      };
+      //public
+      return{
+        API_BASE: API_BASE,
+        setAuthHeader: setAuthHeader
+      };
+  })(jQuery);
+
+    //iife immedietly invoked function expression
+
+
+
+
+
+
+
+
+
+
+
 	$("#testAPI").on("click", function(){
 		console.log("It is working");
   });
