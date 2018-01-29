735434028201daf525edaf5f8e0b400430bf2464
Adam Smith <adamws33@gmail.com>
2018-01-28 22:06:41 -0500

    definition route

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db.js')
-var User = sequelize.import('./models/user.js');
+
+// var User = sequelize.import('./models/user.js');
 
 //created the table in postgres
 //mathces the model we defied 
 // doesnt drop the db 
-User.sync();
+// User.sync();
 // //**********DANGER************DANGER************DANGER********
 // // **********DANGER************DANGER************DANGER********
 // // ****************** User.sync({force:true}); *******************
 // // ***** DANGER THIS CODE WILL DROP ALL DATA STORED IN THE DB ****
 // // **********DANGER************DANGER************DANGER******** 
 // // **********DANGER************DANGER************DANGER********
+
+sequelize.sync();
 
 app.use(bodyParser.json());
 // saying to require the addition of headers from this file to have additional information sent along 
@@ -24,10 +28,10 @@ app.use(require('./middleware/validate-session'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/login', require('./routes/session'));
 app.use('/api/definition', require('./routes/definition'));
-app.use('/api/log', require('./routes/log'));
+// app.use('/api/log', require('./routes/log'));
 // sending the string "hello world" to the api/test location for the client to pull through a header
-app.use('/api/test', function(req,res){
-  res.send("Hello World")
+// app.use('/api/test', function(req,res){
+//   res.send("Hello World")
 })
 
 app.listen(3000, function(){
